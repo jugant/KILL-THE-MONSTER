289703828c71af44d9e709336e4b3cef56b2ae32
jugant <73983043+jugant@users.noreply.github.com>
2021-01-31 20:16:06 +0530

    Add files via upload

---
+++ Throw.js
@@ -0,0 +1,30 @@
+class Throw {
+    constructor(bodyA, pointB) {
+      var options = {
+        bodyA: bodyA,
+        pointB: pointB,
+        stiffness: 1.2,
+        length: 250,
+      };
+  
+      this.pointB = pointB;
+      this.throw = Constraint.create(options);
+      World.add(world, this.throw);
+    }
+    fly(){
+      this.throw.bodyA=null;
+    }
+
+    launch(bodyA){
+      this.throw.bodyA=bodyA;
+    }
+
+    display(){
+      
+    }
+  
+    
+    
+    
+  }
+  
